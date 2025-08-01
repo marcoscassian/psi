3af937b6542e331c9fda095c840343a08e8d6633
marcoscassian <161905696+marcoscassian@users.noreply.github.com>
2025-08-01 10:13:26 -0300

    Update app.py

--- B2/revisao/app.py
+++ B2/revisao/app.py
@@ -5,39 +5,31 @@ import sqlite3
 
 
 def obter_conexao():
-    conn = sqlite3.connect('jogo.db')
+    conn = sqlite3.connect('banco.db')
     conn.row_factory = sqlite3.Row
     return conn
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
 
-@app.route('/personagens')
-def personagens():
-    conn = obter_conexao()
-    SQL = "SELECT * FROM jogo"
-    lista = conn.execute(SQL).fetchall()
-    conn.close()
-    return render_template("personagens.html", lista=lista)
-  
-@app.route('/novo', methods=['GET', 'POST'])
-def novo():
     if request.method == "POST":
-        nomepersonagem = request.form['nomepersonagem']
-        jogoorigem = request.form['jogoorigem']
-        habilidade = request.form['habilidade']
+        nome = request.form['nome']
 
         conn = obter_conexao()
 
-        SQL = "INSERT INTO jogo(nomepersonagem, jogoorigem, habilidade) VALUES (?, ?, ?)"
-        conn.execute(SQL, (nomepersonagem, jogoorigem, habilidade))
+        SQL = "INSERT INTO users(nome) VALUES (?)"
+        conn.execute(SQL, (nome,))
         conn.commit()
         conn.close()
 
 
-        return redirect(url_for('personagens'))
+        return redirect(url_for('index'))
 
-    return redirect(url_for('novo.html'))
+    conn = obter_conexao()
+    SQL = "SELECT * FROM users"
+    lista = conn.execute(SQL).fetchall()
+    conn.close()
+
+    return render_template('index.html', lista=lista)
