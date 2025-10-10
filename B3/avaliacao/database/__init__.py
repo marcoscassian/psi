9a4895cf1ba535703ee00341312e7f0b082ff3d4
Marcos Cassiano <marcozcassiano@gmail.com>
2025-10-10 11:46:52 -0300

    Delete B3/avaliacao directory

--- B3/avaliacao/database/__init__.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, DeclarativeBase
-
-engine = create_engine('sqlite:///app.db')
-session = Session(bind=engine)
-
-class Base(DeclarativeBase):
-    pass
